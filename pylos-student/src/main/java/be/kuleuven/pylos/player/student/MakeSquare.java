1e00307b800a4406f2651deee8e83678ee03817d
Järn <38694005+JosPineut@users.noreply.github.com>
2019-03-04 15:24:12 +0100

    Finished MakeSquare

--- pylos-student/src/main/java/be/kuleuven/pylos/player/student/MakeSquare.java
+++ pylos-student/src/main/java/be/kuleuven/pylos/player/student/MakeSquare.java
@@ -1,25 +1,57 @@
 package be.kuleuven.pylos.player.student;
 
-import be.kuleuven.pylos.game.PylosBoard;
-import be.kuleuven.pylos.game.PylosGameIF;
+import be.kuleuven.pylos.game.*;
+import be.kuleuven.pylos.player.PylosPlayer;
 
 public class MakeSquare implements Move{
+    private PylosLocation bestPlace;
+    private int bestScore=100;
 
     @Override
-    public void doMove(PylosGameIF game, PylosBoard board) {
+    public void doMove(PylosGameIF game, PylosBoard board, PylosPlayer player) {
 
+        PylosSphere myReserveSphere = board.getReserve(player);
+        game.moveSphere(myReserveSphere,bestPlace);
     }
 
     @Override
-    public boolean isPossible() {
+    public boolean isPossible(PylosGameIF game, PylosBoard board, PylosPlayer player) {
 
+        PylosSphere[] mySpheres = board.getSpheres(player);
+        PylosSquare[] squares = board.getAllSquares();
+        boolean possible=false;
+        int score=100;
 
+        for(PylosSquare ps:squares){
+            if(ps.getInSquare(player) == 3){
+                PylosLocation[] pls =ps.getLocations();
+                for(PylosLocation pl:pls){
+                    if(!pl.isUsed() && pl.isUsable()){
+                        possible= true;
+                        score=getScore(game,board,player,pl);
+                        if(score < bestScore){
+                            bestScore= score;
+                            bestPlace=pl;
+                        }
+                    }
+                }
+            }
+        }
 
-        return false;
+        return possible;
     }
 
     @Override
-    public int getScore() {
-        return 0;
+    public int getScore(PylosGameIF game, PylosBoard board, PylosPlayer player,PylosLocation pl) {
+
+        int score=0;
+        PylosSquare[] squares = board.getAllSquares();
+        for(PylosSquare ps:squares){
+            if(ps.getInSquare(player)==0 && ps.getInSquare(player.OTHER) > 0){
+                score++;
+            }
+        }
+
+        return score;
     }
 }
--- pylos-student/src/main/java/be/kuleuven/pylos/player/student/Move.java
+++ pylos-student/src/main/java/be/kuleuven/pylos/player/student/Move.java
@@ -2,11 +2,13 @@ package be.kuleuven.pylos.player.student;
 
 import be.kuleuven.pylos.game.PylosBoard;
 import be.kuleuven.pylos.game.PylosGameIF;
+import be.kuleuven.pylos.game.PylosLocation;
+import be.kuleuven.pylos.player.PylosPlayer;
 
 public interface Move {
 
-    public void doMove(PylosGameIF game, PylosBoard board);
-    public boolean isPossible();
-    public int getScore();
+    public void doMove(PylosGameIF game, PylosBoard board, PylosPlayer player);
+    public boolean isPossible(PylosGameIF game, PylosBoard board, PylosPlayer player);
+    public int getScore(PylosGameIF game, PylosBoard board, PylosPlayer player, PylosLocation pl);
     
 }
