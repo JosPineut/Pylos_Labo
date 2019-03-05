8a799a694f642824f976ebf2d6937e824de74b61
Jarne Clarysse <Jarne.clarysse@hotmail.be>
2019-03-05 22:55:45 +0100

    regularMove + remove1

--- pylos-student/src/main/java/be/kuleuven/pylos/player/student/RegularMove.java
+++ pylos-student/src/main/java/be/kuleuven/pylos/player/student/RegularMove.java
@@ -3,17 +3,20 @@ package be.kuleuven.pylos.player.student;
 import be.kuleuven.pylos.game.*;
 import be.kuleuven.pylos.player.PylosPlayer;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RegularMove implements Move {
 
-    int[] bestMoveScore = new int[2];
+    int[] bestMoveScore = new int[4];
 
 
     @Override
     public void doMove(PylosGameIF game, PylosBoard board, PylosPlayer player) {
+        bestMoveScore[0] = Integer.MAX_VALUE;
+        bestMoveScore[1] = Integer.MIN_VALUE;
+        bestMoveScore[2] = Integer.MIN_VALUE;
+        bestMoveScore[3] = Integer.MIN_VALUE;
 
         //System.out.println(board.getNumberOfSpheresOnBoard() );
         if (board.getNumberOfSpheresOnBoard() == 0) {
@@ -21,7 +24,7 @@ public class RegularMove implements Move {
         } else {
             regularMove(game, board, player);
         }
-        System.out.println(scoreOpponent(game, board, player));
+
     }
 
     public void openingMove(PylosGameIF game, PylosBoard board, PylosPlayer player) {
@@ -32,39 +35,89 @@ public class RegularMove implements Move {
     }
 
     public void regularMove(PylosGameIF game, PylosBoard board, PylosPlayer player) {
+        PylosLocation bestLocation = null;
         PylosLocation[] allLocations = board.getLocations();
         shuffleArray(allLocations);
         PylosSphere myReserveSphere = board.getReserve(player);
         int i = 0;
 
-
+        //System.out.println("##########################################################################");
         while (i < allLocations.length) {
             if (allLocations[i].isUsable()) {
-                game.moveSphere(myReserveSphere, allLocations[i]);
-                i = allLocations.length;
+
+                board.add(myReserveSphere, allLocations[i]);
+                if (score(game, board, player,myReserveSphere)) {
+                    bestLocation = allLocations[i];
+                }
+                board.remove(myReserveSphere);
             }
             i++;
         }
+
+        game.moveSphere(myReserveSphere, bestLocation);
     }
 
-    public int scoreOpponent(PylosGameIF game, PylosBoard board, PylosPlayer player) {
-        int score = 0;
+    public boolean score(PylosGameIF game, PylosBoard board, PylosPlayer player, PylosSphere reserveSphere) {
+        int score1 = 0;
+        int score2 = 0;
+        int score3 = Integer.MIN_VALUE;
+        int score4 = Integer.MIN_VALUE;
         PylosSquare[] squares = board.getAllSquares();
         for (PylosSquare square : squares) {
-            if(square.getInSquare(player)==0 && square.getInSquare(player.OTHER)!=0){
-                score+=1;
+            if (square.getInSquare(player) == 0 && square.getInSquare(player.OTHER) != 0) {
+                score1 += 1;
+
+            } else if (square.getInSquare(player) != 0 && square.getInSquare(player.OTHER) == 0) {
+                score2 += 1;
             }
+            if (containsLocation(square, reserveSphere)) {
+                if (score3 < square.getInSquare(player.OTHER)) {
+                    score3 = square.getInSquare(player.OTHER);
+                }
+                if (score4 < square.getInSquare(player)) {
+                    score4 = square.getInSquare(player);
+                }
+            }
+
         }
-        return score;
+
+        //System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        //System.out.println("1: "+score1+" 2: "+score2+" 3: "+score3+" 4: "+score4);
+        if (score1 < bestMoveScore[0]) {
+            bestMoveScore[0] = score1;
+            return true;
+        } else if (score1 == bestMoveScore[0] && score2 > bestMoveScore[1]) {
+            bestMoveScore[1] = score2;
+            return true;
+        } else if (score1 == bestMoveScore[0] && score2 == bestMoveScore[1] && score3 > bestMoveScore[2]) {
+            bestMoveScore[2] = score3;
+            return true;
+        } else if(score1 == bestMoveScore[0] && score2 == bestMoveScore[1] && score3 == bestMoveScore[2] && score4 > bestMoveScore [3]) {
+            bestMoveScore[3] = score4;
+            return true;
+        } else if (score1 == bestMoveScore[0] && score2 == bestMoveScore[1] && score3 == bestMoveScore[2] && score4 == bestMoveScore[3]) {
+            Random rand = new Random();
+            return rand.nextInt(4) == 2;
+        }
+        return false;
     }
 
-    @Override
-    public boolean isPossible(PylosGameIF game, PylosBoard board, PylosPlayer player) {
+    public boolean containsLocation(PylosSquare square, PylosSphere sphere) {
+        for (PylosLocation loc : square.getLocations()) {
+            if (loc == sphere.getLocation()) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public int getScore(PylosGameIF game, PylosBoard board, PylosPlayer players) {
+    public boolean isPossible(PylosGameIF game, PylosBoard board, PylosPlayer player) {
+        return true;
+    }
+
+    @Override
+    public int getScore(PylosGameIF game, PylosBoard board, PylosPlayer player, PylosLocation pl) {
         return 0;
     }
 
